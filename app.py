6ceb875b35eafa2b83c6b75fd895ac990b88fca1
tonish-p <tonish.p@outlook.com>
2026-03-11 20:41:43 +0530

    veriables
    made a count of students, teachers and course name

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello,tonish")
+students_count = 1000
+theachers_count = 500
+rating = 4.99
+is_published = True
+course_name = "python programming"
+print("1054_all_together: ")
+print(students_count + theachers_count + 4)
+print(students_count - 54 + theachers_count + 4)
